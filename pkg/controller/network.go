230e30fc33dee6a6665b8b99a8c8927e6d6f7db3
Manuel Hutter <manuel@hutter.io>
2021-06-16 11:03:12 +0200

    Move reconcilation to own function
    This will make testing much easier
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- pkg/controller/network.go
+++ pkg/controller/network.go
@@ -1,13 +1,11 @@
 package controller
 
 import (
-	"context"
 	"time"
 
 	v1 "github.com/openshift/api/network/v1"
 	"github.com/openshift/client-go/network/clientset/versioned"
 	"github.com/openshift/client-go/network/informers/externalversions"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 )
@@ -43,49 +41,11 @@ func (c *Controller) createNetworkInformer() {
 }
 
 func (c *Controller) AddHostSubnet(hs *v1.HostSubnet) {
-	c.reconcileSubnet(hs)
+	ReconcileSubnet(hs, c.cidrs, c.machines.Get, c.hostSubnetClient.Update)
 }
 
 func (c *Controller) UpdateHostSubnet(_, hs *v1.HostSubnet) {
-	c.reconcileSubnet(hs)
+	ReconcileSubnet(hs, c.cidrs, c.machines.Get, c.hostSubnetClient.Update)
 }
 
 func (c *Controller) DeleteHostSubnet(hs *v1.HostSubnet) {}
-
-func (c *Controller) reconcileSubnet(hs *v1.HostSubnet) {
-	klog.V(8).Infof("HostSubnet<%s>: Reconcile", hs.Name)
-	machine, err := c.machines.Get(hs.Name)
-	if err != nil {
-		klog.Errorf("HostSubnet<%s>: get machine: %s", hs.Name, err)
-		return
-	}
-
-	machineset, ok := machine.Labels[MachinesetLabel]
-	if !ok {
-		klog.Errorf("HostSubnet<%s>: no '%s' label on machine", hs.Name, MachinesetLabel)
-		return
-	}
-
-	if !c.cidrs.Exists(machineset) {
-		klog.V(8).Infof("HostSubnet<%s>: No or empty entry in CIDR cache, skipping", hs.Name)
-		return
-	}
-
-	actual := hs.EgressCIDRs
-	if c.cidrs.EqualCIRDs(machineset, actual) {
-		klog.V(8).Infof("HostSubnet<%s>: Already matches desired value, skipping", hs.Name)
-		return
-	}
-
-	desired := c.cidrs.Get(machineset)
-	klog.Infof("HostSubnet<%s>: Out of date, updating.", hs.Name)
-	klog.Infof("HostSubnet<%s>: Old value: %v", hs.Name, actual)
-	klog.Infof("HostSubnet<%s>: New value: %v", hs.Name, desired)
-	hs.EgressCIDRs = desired
-	_, err = c.hostSubnetClient.Update(context.Background(), hs, metav1.UpdateOptions{
-		FieldManager: "openshift-machineset-egress-cidr-operator",
-	})
-	if err != nil {
-		klog.Error("HostSubnet<%s>: updating: %s", hs.Name, err)
-	}
-}
---
+++ pkg/controller/reconcile.go
@@ -0,0 +1,59 @@
+package controller
+
+import (
+	"context"
+
+	v1 "github.com/openshift/api/network/v1"
+	"github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
+)
+
+type MachineGetter func(name string) (*v1beta1.Machine, error)
+type HostSubnetUpdater func(ctx context.Context, hostSubnet *v1.HostSubnet, opts metav1.UpdateOptions) (*v1.HostSubnet, error)
+
+func ReconcileSubnet(
+	hs *v1.HostSubnet,
+	cidrs *CIDRMap,
+	getMachine MachineGetter,
+	updateHostSubnet HostSubnetUpdater,
+) string {
+	klog.V(8).Infof("HostSubnet<%s>: Reconcile", hs.Name)
+	machine, err := getMachine(hs.Name)
+	if err != nil {
+		klog.Errorf("HostSubnet<%s>: get machine: %s", hs.Name, err)
+		return "error getMachine: " + err.Error()
+	}
+
+	machineset, ok := machine.Labels[MachinesetLabel]
+	if !ok {
+		klog.Errorf("HostSubnet<%s>: no '%s' label on machine", hs.Name, MachinesetLabel)
+		return "error: no machineset label"
+	}
+
+	if !cidrs.Exists(machineset) {
+		klog.V(8).Infof("HostSubnet<%s>: No or empty entry in CIDR cache, skipping", hs.Name)
+		return "no cidr entry"
+	}
+
+	actual := hs.EgressCIDRs
+	if cidrs.EqualCIRDs(machineset, actual) {
+		klog.V(8).Infof("HostSubnet<%s>: Already matches desired value, skipping", hs.Name)
+		return "up to date"
+	}
+
+	desired := cidrs.Get(machineset)
+	klog.Infof("HostSubnet<%s>: Out of date, updating.", hs.Name)
+	klog.Infof("HostSubnet<%s>: Old value: %v", hs.Name, actual)
+	klog.Infof("HostSubnet<%s>: New value: %v", hs.Name, desired)
+	hs.EgressCIDRs = desired
+	_, err = updateHostSubnet(context.Background(), hs, metav1.UpdateOptions{
+		FieldManager: "openshift-machineset-egress-cidr-operator",
+	})
+	if err != nil {
+		klog.Error("HostSubnet<%s>: updating: %s", hs.Name, err)
+		return "error update hostsubnet: " + err.Error()
+	}
+
+	return "updated"
+}
